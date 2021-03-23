71357b968727a2cd239866fef1f83216c7abfef0
fengjiansong <fengjiansong@youjiajk.com>
2021-03-23 10:31:52 +0800

    添加dockerfile

---
+++ src/main/java/cn/funjson/wechatapiboot/controller/TestController.java
@@ -0,0 +1,19 @@
+package cn.funjson.wechatapiboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author fengjiansong
+ * @Date 2021/3/23 10:30 上午
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/home")
+    public String sayHello(){
+        return "hello";
+    }
+}
