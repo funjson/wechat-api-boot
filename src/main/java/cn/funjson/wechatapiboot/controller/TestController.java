08748fbed48e09259e6c5274f9f8dbbc7b653a03
fengjiansong <fengjiansong@youjiajk.com>
2021-04-04 20:43:20 +0800

    测试

--- src/main/java/cn/funjson/wechatapiboot/controller/TestController.java
+++ src/main/java/cn/funjson/wechatapiboot/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.funjson.wechatapiboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
     @GetMapping("/home")
     public String sayHello(){
+        log.info("hello");
         return "hello 11";
     }
 }
